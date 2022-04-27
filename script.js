b26bf7027e35c6c5a43d4748fa1aee554917e101
Chris Rumler <cgrumler@gmail.com>
2022-04-27 15:35:22 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.hamburger_menu_container');
+const navMenu = document.querySelector('.nav__menu');
+const closeHamburger = document.querySelector('.close_hamburger_container');
+
+hamburger.addEventListener('click', mobileMenu);
+
+// Toggles the Hamburger Menu button
+function mobileMenu() {
+  hamburger.classList.toggle('hidden');
+  navMenu.classList.toggle('active');
+  closeHamburger.classList.toggle('active');
+};
+
+closeHamburger.addEventListener('click', closeMobileMenu);
+
+// Toggles the Close Menu button
+function closeMobileMenu() {
+  hamburger.classList.toggle('hidden');
+  navMenu.classList.toggle('active');
+  closeHamburger.classList.toggle('active');
+};
